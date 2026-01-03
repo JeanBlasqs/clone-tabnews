4f42ee908121ef7929fa7cafb32c10a09807648a
Jean Blasques <151785367+JeanBlasqs@users.noreply.github.com>
2026-01-03 15:36:43 +0000

    adiciona scripts lint:fix e lint:check

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 implementação do tabsnews para o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-export default function Home() {
-  return (
-    <>
-      <h1>사랑해요</h1>
-    </>
-  )
+function Home() {
+  return <h1>사랑해요</h1>;
 }
+
+export default Home;
