9792462c9ff3ddbf9c73e46689f8b07b24fd7a07
Jean Blasques <151785367+JeanBlasqs@users.noreply.github.com>
2026-08-07 22:59:27 +0000

    feat: adiciona validações de username/endpoint dinamico

--- infra/errors.js
+++ infra/errors.js
@@ -60,6 +60,26 @@ export class ValidationError extends Error {
     };
   }
 }
+export class NotFoundError extends Error {
+  constructor({ cause, message, action } = {}) {
+    super(message || "Não foi possível encontrar esse recurso no sistema.", {
+      cause,
+    });
+
+    this.name = "NotFoundError";
+    this.action = action || "Verifique os parâmetros.";
+    this.status_code = 404;
+  }
+
+  toJson() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.status_code,
+    };
+  }
+}
 
 export class MethodNotAllowedError extends Error {
   constructor({ cause } = {}) {
