616542a1cc12637dbd1097f8b94274beba3f6494
Jean Blasques <151785367+JeanBlasqs@users.noreply.github.com>
2026-08-21 20:25:18 +0000

    feat: refatora criação de usuário para ser usado no orchestrator

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,6 +1,7 @@
 import retry from "async-retry";
 import database from "infra/database.js";
 import migrator from "models/migrator.js";
+import user from "models/user.js";
 
 async function waitForAllServices() {
   await waitForWebServer();
@@ -28,10 +29,15 @@ async function runPendingMigrations() {
   await migrator.runPendingMigrations();
 }
 
+async function createUser(userObject) {
+  await user.create(userObject);
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
   runPendingMigrations,
+  createUser,
 };
 
 export default orchestrator;
