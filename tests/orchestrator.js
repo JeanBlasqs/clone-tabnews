61ac472eb6e291b4a4b33285450a70f0fb9d3bd1
Jean Blasques <151785367+JeanBlasqs@users.noreply.github.com>
2026-08-24 23:41:40 +0000

    feat: add refactor to user test and use orchestrator.js

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,4 +1,5 @@
 import retry from "async-retry";
+import { faker } from "@faker-js/faker";
 import database from "infra/database.js";
 import migrator from "models/migrator.js";
 import user from "models/user.js";
@@ -30,7 +31,12 @@ async function runPendingMigrations() {
 }
 
 async function createUser(userObject) {
-  await user.create(userObject);
+  return await user.create({
+    username:
+      userObject?.username || faker.internet.username().replace(/[_.-]/g, ""),
+    email: userObject?.email || faker.internet.email(),
+    password: userObject?.password || "validpassword",
+  });
 }
 
 const orchestrator = {
